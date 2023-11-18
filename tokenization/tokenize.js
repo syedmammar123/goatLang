73891b9801a098ac165ca3008ce1bb5e5829bdad
Usman Siddiqui <siddiquiusman328@gmail.com>
2023-11-18 15:38:06 +0500

    ..

--- tokenization/tokenize.js
+++ tokenization/tokenize.js
@@ -2,14 +2,108 @@ import { isValidName } from "../helpers/token-checks.js";
 import { keywords } from "../environment/environment.js";
 
 
-let code = "global const a = 'hello world'";
+let code = "global const number = 2304";
+
+
+function tokenize (code ){
+    let i = 0;
+    let tokens = []
+    let char=""
+    let isQuotationOpened=false;
+
+
+
+    while (i<code.length){
+        if (/^[a-zA-Z0-9_$@#]$/.test(code[i])){
+            char = char + code[i]
+            i++
+        }
+        console.log(char)
+        if (code[i] === " "){
+            while (code[i] === " "){
+                i++
+            }
+            if (keywords.includes(char)){
+                tokens.push({
+                    type:'keyword',
+                    value:char
+                })
+                char  = ""
+            }else if((code[i] !== "=") && char !== "" && (!keywords.includes(char))){
+                console.log(char,"char")
+                tokens.push({
+                    type:'identifier',
+                    value:char
+                })
+                char  = ""
+
+            }
+        }
+        if (code[i] === "="){
+                tokens.push({
+                    type:'identifier',
+                    value:char
+                })
+                tokens.push({
+                    type:'equality',
+                    value: "="
+                })
+            char = ""
+            i++
+
+        }
+        if (code[i] === "'"){
+            i++;
+            while (code[i] !== "'" ){
+                char = char + code[i];
+                i++
+            }
+            i++;
+            tokens.push({
+                type:"string",
+                value:char
+            })
+
+        }
+        
+        if (!isNaN(Number(code[i])) && char === ""){
+            let decimalCount = 0;
+            let num = ""
+            while ((!isNaN(Number(code[i])) || code[i] === ".")){
+                console.log(code[i])
+                if (code[i] === "."){
+                    decimalCount++
+                }
+                if (decimalCount > 1){
+                    throw new Error("Invalid number")
+                }
+                num = num + code[i]
+                i++
+            }
+            tokens.push({
+                type:'Number',
+                value:Number(num)
+            })
+            i++
+        }
+    }
+    return tokens;
+}
+
+console.log(tokenize(code))
+
+
+
+
+
+
 
 let i = 0;
 let tokens = []
 let char = ""
 let isQuotationOpened = false;
 
-while (i < code.length){
+while (i < tokens.length){
     if (code[i] !== "=" && code[i] !== "'" && code[i] !== " " ){
         char = char + code[i]; 
     }
@@ -64,4 +158,4 @@ while (i < code.length){
 }
 
 
-console.log(tokens)
+//console.log(tokens)
