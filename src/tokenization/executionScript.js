e43eb8981e408cbf71162df6da9ba2c25b91d81d
Usman Siddiqui <siddiquiusman328@gmail.com>
2024-01-03 00:05:51 +0500

    ...

--- src/tokenization/executionScript.js
+++ src/tokenization/executionScript.js
@@ -4,6 +4,18 @@ import fs from 'fs'
 import path from 'path'
 import { generateJsCode } from './main.js'
 
+
+function filePathToUrl(filePath) {
+  const forwardSlashPath = filePath.replace(/\\/g, '/');
+
+  const fileUrl = new URL(`file://${forwardSlashPath}`);
+
+  return fileUrl.href;
+}
+
+
+
+
 let filePath = path.join(process.cwd(),process.argv[2])
 
 const code = fs.readFileSync(filePath,{encoding:"utf8"})
@@ -12,8 +24,8 @@ const jsCode = generateJsCode(code)
 
 fs.writeFileSync('./temp.js', jsCode)
 
-
-import('../../temp.js')
+const destPath = path.join(process.cwd(),'./temp.js')
+import(filePathToUrl(destPath))
   .catch((error) => {
     console.error('Error importing temp.js:', error);
   })
