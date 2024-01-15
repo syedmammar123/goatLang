9b2fa38479b6711b1837728f6f5cfc0b629703e0
Syed Muhammad Ammar <syedmammar123@gmail.com>
2024-01-15 22:38:04 +0500

    minor update

--- src/Parsing/ast.js
+++ src/Parsing/ast.js
@@ -300,6 +300,18 @@ export const codeTokenizer = (code)=>{
     return tokenize(code)
 }
 
+export const GetTAC = (goatCode)=>{
+    try {
+        let tokens = tokenize(goatCode)
+        let ast = generateAst(tokens)
+
+        let code = generate.default(ast).code
+        return [tokens, ast, code]
+    } catch (e) {
+        console.log(e.message)
+    }
+}
+
 // console.log('Input')
 // console.log(code)
 // console.log('\n')
